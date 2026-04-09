27fb7ab5a1cb83f25a30914fd9ec844dc7e24bf2
Mori Aryan <moriaryan2024@gamil.com>
2026-04-09 16:34:06 +0530

    Fixed backend server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,30 @@
-import app from "./app.js";
-import env from "./config/env.js";
-
-app.listen(env.port, () => {
-  console.log(`Odoo POS Cafe API running on port ${env.port}`);
+process.on("unhandledRejection", (reason) => {
+  console.error("Unhandled promise rejection:", reason);
 });
+
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught exception:", error);
+});
+
+async function startServer() {
+  try {
+    const [{ default: app }, { default: env }] = await Promise.all([
+      import("./app.js"),
+      import("./config/env.js"),
+    ]);
+
+    const server = app.listen(env.port, () => {
+      console.log(`Odoo POS Cafe API running on port ${env.port}`);
+    });
+
+    server.on("error", (error) => {
+      console.error("HTTP server error:", error);
+      process.exit(1);
+    });
+  } catch (error) {
+    console.error("Fatal startup error:", error);
+    process.exit(1);
+  }
+}
+
+void startServer();
